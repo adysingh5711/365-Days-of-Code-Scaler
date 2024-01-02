232ef2bebc799ebed3b9df3f8f419ab2406fd29a
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-01-02 22:57:35 +0530

    Create Day1.c

---
+++ Day 1-30/Day1.c
@@ -0,0 +1,16 @@
+// https://www.interviewbit.com/problems/loopcmpl/
+
+
+int a = 0, b = 0;    
+        for (i = 0; i < N; i++) {
+            a = a + rand();  
+        }
+        for (j = 0; j < M; j++) {
+            b = b + rand();
+        }
+
+// Time complexity: O(N + M)
+// Space complexity: O(1)
+
+
+
