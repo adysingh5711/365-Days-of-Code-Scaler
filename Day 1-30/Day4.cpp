4bb5a13fb8fced9aa40b321b70d903f4b11ab40c
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-01-06 21:58:55 +0530

    Create Day4.cpp

---
+++ Day 1-30/Day4.cpp
@@ -0,0 +1,16 @@
+https://www.interviewbit.com/problems/smaller-or-equal-elements/discussion/
+
+
+int Solution::solve(vector<int> &A, int B) {
+    int start = 0;
+    int end = A.size()-1;
+    while(start<=end){
+        int mid = start + (end -start)/2;
+        if(A[mid]>B){
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+    return start;
+}
