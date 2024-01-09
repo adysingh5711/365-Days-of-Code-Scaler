2dad35cb4b671d5fd9f5cb62532270f42b680adf
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-01-10 01:50:36 +0530

    Create Day8.cpp

---
+++ Day 1-30/Day8.cpp
@@ -0,0 +1,12 @@
+// https://www.interviewbit.com/problems/sell-items/
+
+
+/**
+ * @input A : Integer
+ * @input B : Integer
+ * 
+ * @Output Integer
+ */
+int solve(int A, int B) {
+    return ceil((double)B / (A * 5));
+}
