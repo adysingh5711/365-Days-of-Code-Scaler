54a57ebd2e74620bec64c22e620414da203a9cff
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-01-14 19:19:11 +0530

    Create Day12.cpp

---
+++ Day 1-30/Day12.cpp
@@ -0,0 +1,17 @@
+// https://www.interviewbit.com/problems/check-palindrome/
+
+
+
+int Solution::solve(string A) {
+     unordered_map<char, int> freq;
+    for (int i=0;i<A.size();i++) {
+        freq[A[i]]++;
+    }
+    int odd_count = 0;
+    for (auto it : freq) {
+        if (it.second % 2 != 0) {
+            odd_count++;
+        }
+    }
+    return odd_count <= 1;
+}
