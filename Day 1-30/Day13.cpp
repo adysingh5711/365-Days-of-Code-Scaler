11e4c9e7d3b62716b8753fb2f2893abfdba3a45c
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-01-14 19:24:20 +0530

    Create Day13.cpp

---
+++ Day 1-30/Day13.cpp
@@ -0,0 +1,21 @@
+// https://www.interviewbit.com/problems/clear-the-array/discussion/p/easy-c-approach-pattern/428482/2419/
+
+
+int Solution::solve(vector<int> &A) {
+    map<int,int> deletesub;
+    
+    //Just see that the element which occurs the most will cause the problem.
+    //So print its number of occurences.
+    
+    for(int i=0;i<A.size();i++)
+    {
+        deletesub[A[i]]++;
+    }
+    int attempts=0;
+    for(auto it: deletesub)
+    {
+        if(it.second>attempts)
+        attempts=it.second;
+    }
+    return attempts;
+}
