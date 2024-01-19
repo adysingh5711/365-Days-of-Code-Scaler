93116971d5335fe3bea2d218bda5cb2d13ff4aa1
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-01-19 23:25:10 +0530

    Create Day18.cpp

---
+++ Day 1-30/Day18.cpp
@@ -0,0 +1,18 @@
+// https://www.interviewbit.com/problems/find-variance/discussion/p/easy-c-soln-pop-back/451674/2593/
+
+
+string Solution::solve(vector<int> &a) {
+    int n=a.size();
+    double sum=0;
+    for(int x: a) sum+=x;
+    double avg=sum/n;
+    double var=0;
+    for(int x:a) var+=pow(x-avg,2);
+    var/=n;
+    var=(var*100+0.5);
+    int ans=var;
+    var=ans/100.0;
+    string res=to_string(var);
+    for(int i=0;i<4;i++) res.pop_back();
+    return res;
+}
