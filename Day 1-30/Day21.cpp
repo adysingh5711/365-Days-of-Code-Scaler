cc46b7567aa8d860c6cb3f04e578b32030a1a0fc
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-01-22 23:41:39 +0530

    Create Day21.cpp

---
+++ Day 1-30/Day21.cpp
@@ -0,0 +1,14 @@
+// https://www.interviewbit.com/problems/frequency-of-characters/
+
+
+vector<int> Solution::solve(string A) {
+    vector<int> result(26, 0); // Initialize result vector with 26 zeros
+
+    for (char c : A) {
+        if (isalpha(c)) { // Check if the character is an alphabet letter
+            result[c - 'a']++; // Increment the corresponding element in the result vector
+        }
+    }
+
+    return result;
+}
