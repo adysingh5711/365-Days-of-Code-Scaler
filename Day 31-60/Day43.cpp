e0012219a3d392d7d6b8f2fcefed50d63a5de837
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-02-13 21:50:36 +0530

    Create Day43.cpp

---
+++ Day 31-60/Day43.cpp
@@ -0,0 +1,19 @@
+// https://www.interviewbit.com/problems/majority-element/
+
+
+int Solution::majorityElement(const vector<int>& nums) {
+    int n = nums.size();
+    int element=nums[0];
+    int count=0;
+    for(int i=0; i<n; i++){
+        if(count == 0)
+            element = nums[i];
+           
+        if(element == nums[i])
+            count += 1;
+        else
+            count -= 1;
+    }
+   
+    return element;
+}
