dab157ab1736dcf8459ae6ef4144dad903b34e30
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-02-15 23:37:37 +0530

    Update Day45.cpp

--- Day 31-60/Day45.cpp
+++ Day 31-60/Day45.cpp
@@ -1,13 +1,38 @@
-// https://www.geeksforgeeks.org/problems/castle-run3644/1
+// https://www.interviewbit.com/problems/best-time-to-buy-and-sell-stocks-ii/
 
 
-class Solution:
-    def isPossible(self, paths):
-        # Code here
-        for row in paths:
-            sum = 0
-            for col in row:
-                sum = sum + col
-            if sum%2 != 0:
-                return 0
-        return 1
+int f(int index, int buy, const vector<int> &A, vector<vector<int>> &dp)
+{
+
+  if (index >= A.size()) // Days exhausted so no profit.
+
+    return 0;
+
+  if (dp[index][buy] != -1)
+
+    return dp[index][buy];
+
+  int profit;
+
+  if (buy) // If I have a buy option open on that day,( I dont have any stock on me) then either I can buy or let it go
+
+    profit =
+        max(-A[index] + f(index + 1, 0, A, dp), 0 + f(index + 1, 1, A, dp));
+
+  else // If I dont have a buy option then I need to sell what I have or dont sell
+
+    profit =
+        max(+A[index] + f(index + 1, 1, A, dp), 0 + f(index + 1, 0, A, dp));
+
+  return dp[index][buy] = profit;
+}
+
+int Solution::maxProfit(const vector<int> &A)
+{
+
+  int n = A.size();
+
+  vector<vector<int>> dp(n, vector<int>(2, -1));
+
+  return f(0, 1, A, dp);
+}
