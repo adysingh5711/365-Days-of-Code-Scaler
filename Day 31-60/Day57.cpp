9c4c2e9e88312428906c83ca637f3400ae061fe8
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-02-27 23:48:08 +0530

    Create Day57.cpp

---
+++ Day 31-60/Day57.cpp
@@ -0,0 +1,17 @@
+// https://www.interviewbit.com/problems/kth-row-of-pascals-triangle/
+
+
+vector<int> Solution::getRow(int A) {
+        int i = A;
+        vector<int> temp(i+1,1);
+        int nc = 1;
+        int u = i;
+        int d = 1;
+        for(int j=1;j<i;j++){
+            temp[j]  = (temp[j-1] *u)/d;
+            u--;
+            d++;    
+        }
+       
+        return temp;
+}
